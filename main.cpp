53817d9407231b2ab3aab11298f0ad8dbe505bff
DESKTOP-6V36O5F\Gord <737215@unizar.es>
2019-04-18 18:15:48 +0200

    funcion lectura palabra fichero v1

--- main.cpp
+++ main.cpp
@@ -6,7 +6,44 @@
  * -------------------------------
  */
 
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
 #include "Palabra.h"
+
+const int MAX_LONG_FICHERO = 100;
+
+void seleccionarPalabra(const char fichero[], Palabra& p){
+	fstream f;
+	char secuencia[MAX_LETRAS];
+	
+	try {
+		f.open(fichero);
+		f.is_open();
+		
+		int numLineas;
+		f >> numLineas;
+		cout << numLineas << endl;
+		
+		int randomLinea = rand() % numLineas + 1;
+		cout << randomLinea << endl;
+		
+		int direccion = sizeof(Palabra) * (randomLinea - 1);
+		
+		f.seekp(direccion);
+		
+		char linea[4] = "kkk";
+		
+		f << linea << endl;
+		
+		f.getline(secuencia, 128,'\n') ;
+		
+		p = Palabra(secuencia, int(strlen(secuencia)));
+	}
+	catch (exception e) {
+		cerr << " El fichero " << fichero << " no esta disponible " << endl;
+	}
+}
 
 
 /*
@@ -14,8 +51,12 @@
  */
 int main(){
 	
+	srand(time(NULL));
+	
 	char sec1[MAX_LETRAS] = "domingo";
 	char sec2[MAX_LETRAS] = "bicicleta";
+	
+	const char fichero[MAX_LONG_FICHERO] = "palabras_rae.txt";
 	
 	// Priemra instancia del tipo de dato palabra
 	Palabra p1 = Palabra(sec1, 7);
@@ -36,5 +77,12 @@ int main(){
 	cout << " La palabra " << sec3 << " tiene " << p1.obtenerLetras() << " letras " << endl;
 	cout << " La palabra " << sec4 << " tiene " << p2.obtenerLetras() << " letras " << endl;
 	
+	strcpy(sec1,"\0");
+	
+	Palabra p = Palabra(sec1, 0);
+	
+	seleccionarPalabra(fichero, p);
+	
+	cout << " Fin del programa " << endl;
 	return 0;
 }
