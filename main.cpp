1e2916b53e0019fabb70a076737d65de4e264ac0
DESKTOP-6V36O5F\Gord <737215@unizar.es>
2019-04-19 15:55:33 +0200

    main actualizado version struct

--- main.cpp
+++ main.cpp
@@ -13,38 +13,6 @@
 
 const int MAX_LONG_FICHERO = 100;
 
-void seleccionarPalabra(const char fichero[], Palabra& p){
-	fstream f;
-	char secuencia[MAX_LETRAS];
-	
-	try {
-		f.open(fichero);
-		f.is_open();
-		
-		int numLineas;
-		f >> numLineas;
-		cout << numLineas << endl;
-		
-		int randomLinea = rand() % numLineas + 1;
-		cout << randomLinea << endl;
-		
-		int direccion = sizeof(Palabra) * (randomLinea - 1);
-		
-		f.seekp(direccion);
-		
-		char linea[4] = "kkk";
-		
-		f << linea << endl;
-		
-		f.getline(secuencia, 128,'\n') ;
-		
-		p = Palabra(secuencia, int(strlen(secuencia)));
-	}
-	catch (exception e) {
-		cerr << " El fichero " << fichero << " no esta disponible " << endl;
-	}
-}
-
 
 /*
  * Secuenvia de pruebas basicas para probar el TAD Palabra
@@ -56,32 +24,26 @@ int main(){
 	char sec1[MAX_LETRAS] = "domingo";
 	char sec2[MAX_LETRAS] = "bicicleta";
 	
-	const char fichero[MAX_LONG_FICHERO] = "palabras_rae.txt";
-	
 	// Priemra instancia del tipo de dato palabra
-	Palabra p1 = Palabra(sec1, 7);
+	Palabra p1;
+	crearPalabra(sec1, 7, p1);
 	
 	// Segunda instancia del tipo de dato palabra
-	Palabra p2 = Palabra(sec2, 9);
+	Palabra p2;
+	crearPalabra(sec2, 9, p2);
 	
 	// Cadenas de caracteres para almacenar las palabras
 	char sec3[MAX_LETRAS];
 	char sec4[MAX_LETRAS];
 	
 	// Guardado de la palabra <<p1>> en <<sec1>>
-	p1.obtenerPalabra(sec3);
+	obtenerPalabra(sec3, p1);
 	
 	// Guardado de la palabra <<p2>> en <<sec2>>
-	p2.obtenerPalabra(sec4);
+	obtenerPalabra(sec4, p2);
 	
-	cout << " La palabra " << sec3 << " tiene " << p1.obtenerLetras() << " letras " << endl;
-	cout << " La palabra " << sec4 << " tiene " << p2.obtenerLetras() << " letras " << endl;
-	
-	strcpy(sec1,"\0");
-	
-	Palabra p = Palabra(sec1, 0);
-	
-	seleccionarPalabra(fichero, p);
+	cout << " La palabra " << sec3 << " tiene " << obtenerLetras(p1) << " letras " << endl;
+	cout << " La palabra " << sec4 << " tiene " << obtenerLetras(p2) << " letras " << endl;
 	
 	cout << " Fin del programa " << endl;
 	return 0;
